9b479c39d863cc202adcef661f488ddefc7126f1
Adrian Cano <adriancano@MacBook-Air-de-Adrian.local>
2021-07-21 01:06:35 -0300

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,4 @@
-// demo or it did not happen
-
-var demo1 = document.querySelector('#demo-1')
-var demo2 = document.querySelector('#demo-2')
-
-setTimeout(() => { demo1.checked = true }, 500)
-setTimeout(() => { demo1.checked = false}, 1500)
-setTimeout(() => { demo2.checked = true }, 2000)
-setTimeout(() => { demo2.checked = false}, 3000)
-
-
-
-
-//*******************TITLE****************************/
+//*******************TITLE****************************//
 let app = document.getElementById("title");
 var typewriter = new Typewriter(title, {
   loop: false,
